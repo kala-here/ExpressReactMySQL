63a233070701d90796fe45d2d6fdf86d9174b37b
Kala Wetzler <kala.wetzler@gmail.com>
2017-11-05 00:19:17 -0700

    Change default page to display user data - still need to get npm start script to work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  getInitialState(){
+    return {
+      users: []
+    };
+  },
+  componentDidMount(){
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users: users }));
+  },
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="Users">
+        <h1>Users</h1>
+        {this.state.users.map(user =>
+          <div> key={user.id}>{user.email}</div>
+        )}
       </div>
     );
   }
